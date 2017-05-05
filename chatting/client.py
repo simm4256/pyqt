4fa50fe723c1b66e584d7c3eefeca98e5fc76edb
simm4256 <simm4256@naver.com>
2017-05-05 22:17:47 +0900

    0505

--- chatting/client.py
+++ chatting/client.py
@@ -2,12 +2,8 @@ import socket, sys, threading
 from PyQt5 import QtWidgets, uic
 from pys import connect, first
 
-
-
 name=""
 text=""
-
-
 
 if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,16 +11,3 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     w1 = connect.MainWindow(sock)
     sys.exit(app.exec_())
-
-
-
-
-'''
-while True:
-    data = input("text: ")
-
-    if data == ':/quit' or not data:
-        sock.close()
-        break
-    sock.sendall((data).encode('utf8'))
-    '''
--- chatting/pys/connect.py
+++ chatting/pys/connect.py
@@ -1,17 +1,13 @@
 import socket, sys, threading
-from PyQt5 import QtWidgets, uic, QtGui
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
 from pys import first
 
 class MainWindow(QtWidgets.QDialog):
     def __init__(self, sock, parent=None):
-
         QtWidgets.QDialog.__init__(self, parent)
         self.ui = uic.loadUi("uis/connect.ui",self)
         self.ui.show()
-
-        pos = QtGui.QCursor.pos()
-        QtGui.QCursor.setPos(1,0)
-        QtGui.QCursor.setPos(pos)
-
+        self.sock = sock
     def ok(self):
-        print('')
+        w = first.MainWindow(self.sock,self.ui.lineEdit.text(),int(self.ui.lineEdit_5.text()))
+        self.ui.close()
